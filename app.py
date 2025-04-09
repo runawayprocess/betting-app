d121b354a97cc42a98d3f2704131d196900be894
runawayprocess <zacharyhunterjones@gmail.com>
2025-04-09 01:48:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,9 @@ class Bet(db.Model):
 
 # Registration (entry page with donation instructions)
 @app.route("/", methods=["GET", "POST"])
+@app.route("/test")
+def test():
+    return "Hello from deployed Flask!"
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
