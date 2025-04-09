e57f6f34cdbfe36101f653123b371996269b80c5
runawayprocess <zacharyhunterjones@gmail.com>
2025-04-09 02:13:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:/
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 # ----------------------
 # Database Models
